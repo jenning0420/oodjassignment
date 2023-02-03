8d8c999ab739977ba6d1b62c834d4053451897cc
joe <joowe@Flex5>
2023-02-04 01:42:47 +0800

    added delete, view and search

--- src/main/java/Propmanagement/system/ManageUnitSystem.java
+++ src/main/java/Propmanagement/system/ManageUnitSystem.java
@@ -46,7 +46,7 @@ public class ManageUnitSystem {
         return items;
     }
     
-        public Unit getItemByIndex(int index) {
+    public Unit getItemByIndex(int index) {
         List<Unit> listOfItem = getAll();
 
         if (listOfItem.size() >= index) {
@@ -55,7 +55,80 @@ public class ManageUnitSystem {
 
         return null;
     }
-            
+
+    public synchronized boolean delete(String PID) {
+
+        List<Unit> itemList = getAll();
+
+        int indexToBeDeleted = -1;
+        // find the item to be deleted
+        for (int i = 0; i < itemList.size(); i++) {
+            Unit item = itemList.get(i);
+
+            if (item.getPID().equalsIgnoreCase(PID)) {
+                indexToBeDeleted = i;
+            }
+        }
+
+        if (indexToBeDeleted == -1) {
+            return false;
+        }
+        itemList.remove(indexToBeDeleted);
+
+        try {
+            // Delete the entire file
+            Files.delete(Paths.get("src/textFiles/unitManage.txt"));
+        } catch (IOException ex) {
+            Logger.getLogger(ManageUnitSystem.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        // Create a new file and write new data into the file
+        try (PrintWriter pw = new PrintWriter(new FileOutputStream("src/textFiles/unitManage.txt"))) {
+            itemList.forEach(item -> {
+                pw.println(item.getPID() + "," + item.getPUnitNo() + "," + item.getPType() + item.getPPrice() + ",");
+            });
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(ManageUnitSystem.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return true;
+    }
+
+    public synchronized boolean search(String PID) {
+
+        List<Unit> items = getAll();
+
+        int indexToBeSearched = -1;
+        // find the item to be deleted
+        for (int i = 0; i < items.size(); i++) {
+            Unit item = items.get(i);
+
+            if (item.getPID().equalsIgnoreCase(PID)) {
+                indexToBeSearched = i;
+            }
+        }
+
+        if (indexToBeSearched == -1) {
+            return false;
+        }
+
+        try (Scanner scanner = new Scanner(new FileInputStream("src/textFiles/unitManage.txt"))) {
+            while (scanner.hasNextLine()) {
+                String itemLine = scanner.nextLine();
+
+                String itemInfo[] = itemLine.split(",");
+
+                Unit item = new Unit(itemInfo[0], itemInfo[1], itemInfo[2], Double.parseDouble(itemInfo[3]));
+
+                items.add(item);
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(ManageUnitSystem.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return true;        
+    }    
+    
+    
     public void create(Unit item) {
         try (PrintWriter pw = new PrintWriter(new FileOutputStream("src/textFiles/unitManage.txt", true))) {
             pw.println(item.getPID() + "," + item.getPUnitNo() + "," + item.getPType() + "," + item.getPPrice());
