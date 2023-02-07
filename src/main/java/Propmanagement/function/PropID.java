e91528489b0412ded58e28a3473b43a460522a69
joe <joowe@Flex5>
2023-02-07 11:42:05 +0800

    Remove files

---
+++ src/main/java/Propmanagement/function/PropID.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Propmanagement.function;
+
+/**
+ *
+ * @author Admin
+ */
+public class PropID {
+    private String PID;
+    
+    public PropID(String PID) {
+        this.PID = PID;
+    }
+
+    public String getPID() {
+        return PID;
+    }
+
+    public void setPID(String PID) {
+        this.PID = PID;
+    }    
+    
+}
