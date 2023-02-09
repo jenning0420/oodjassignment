e0602a53b1f5deb995c5fb93090efaa1c5b13077
Dllmmimi <mrnobodyjk@gmail.com>
2023-02-10 02:14:23 +0800

    canttt solveee

--- src/main/java/dao/VendorRegisterDaoImpl.java
+++ src/main/java/dao/VendorRegisterDaoImpl.java
@@ -17,8 +17,9 @@ import sng.VendorRegisterSng;
  *
  * @author User
  */
-public class VendorRegisterDaoImpl implements VendorRegisterDao{
-    public boolean vendorRegister(VendorRegisterSng vrs){
+public class VendorRegisterDaoImpl implements VendorRegisterDao {
+
+    public boolean vendorRegister(VendorRegisterSng vrs) {
         try {
             if (!"".equals(vrs.getUsername()) && !"".equals(vrs.getName()) && !"".equals(vrs.getContactNumber()) && !"".equals(vrs.getGender()) && !"".equals(vrs.getPassword())) {
                 File f = new File(vrs.getTextFile());
@@ -28,8 +29,8 @@ public class VendorRegisterDaoImpl implements VendorRegisterDao{
                 BufferedReader br = new BufferedReader(new FileReader(f));
                 Object[] Lines = br.lines().toArray();
                 int i = 0;
-                int id = 0;
-                int propid = 0;
+                int id = 200;
+                int propid = 2000;
                 for (i = 1; i < Lines.length; i++) {
                     String line = Lines[i].toString().trim();
                     String[] row = line.split(",");
@@ -37,8 +38,8 @@ public class VendorRegisterDaoImpl implements VendorRegisterDao{
                     propid = Integer.parseInt(row[0]);
 
                 }
-                int userID = id + 201;
-                int propID = propid + 2001;
+                int userID = id + 1;
+                int propID = propid + 1;
                 FileWriter fw = new FileWriter(f, true);
                 BufferedWriter bw = new BufferedWriter(fw);
                 PrintWriter pw = new PrintWriter(bw);
@@ -47,7 +48,7 @@ public class VendorRegisterDaoImpl implements VendorRegisterDao{
                 pw.close();
                 bw.close();
                 return true;
-            } else{
+            } else {
                 return false;
             }
 
