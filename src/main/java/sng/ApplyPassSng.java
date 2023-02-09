cf2414049b3a500ae3a7747c3bc4bf3c4a3699e5
jenning0420 <80669884+jenning0420@users.noreply.github.com>
2023-02-10 01:28:24 +0800

    visitor pass

---
+++ src/main/java/sng/ApplyPassSng.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sng;
+
+/**
+ *
+ * @author User
+ */
+public class ApplyPassSng {
+    private int passID;
+    private String userID;
+    private String visitorName;
+    private String carPlate;
+    private String unitNo;
+    private String dateIn;
+    private String duration;
+
+    public int getPassID() {
+        return passID;
+    }
+
+    public void setPassID(int passID) {
+        this.passID = passID;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getVisitorName() {
+        return visitorName;
+    }
+
+    public void setVisitorName(String visitorName) {
+        this.visitorName = visitorName;
+    }
+
+    public String getCarPlate() {
+        return carPlate;
+    }
+
+    public void setCarPlate(String carPlate) {
+        this.carPlate = carPlate;
+    }
+
+    public String getUnitNo() {
+        return unitNo;
+    }
+
+    public void setUnitNo(String unitNo) {
+        this.unitNo = unitNo;
+    }
+
+    public String getDateIn() {
+        return dateIn;
+    }
+
+    public void setDateIn(String dateIn) {
+        this.dateIn = dateIn;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+    
+    
+}
