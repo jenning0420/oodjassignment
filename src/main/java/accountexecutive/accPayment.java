e698ee90d446b0c97d7ad59b1a81626dbad31c91
joe <joowe@Flex5>
2023-02-14 17:40:02 +0800

    Added payment pages for acc exec

--- src/main/java/accountexecutive/accPayment.java
+++ src/main/java/accountexecutive/accPayment.java
@@ -8,12 +8,12 @@ package accountexecutive;
  *
  * @author joowe
  */
-public class accexecpayment extends javax.swing.JFrame {
+public class accPayment extends javax.swing.JFrame {
 
     /**
      * Creates new form accexecpayment
      */
-    public accexecpayment() {
+    public accPayment() {
         initComponents();
     }
 
@@ -109,20 +109,21 @@ public class accexecpayment extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(accexecpayment.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(accPayment.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(accexecpayment.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(accPayment.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(accexecpayment.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(accPayment.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(accexecpayment.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(accPayment.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new accexecpayment().setVisible(true);
+                new accPayment().setVisible(true);
             }
         });
     }
